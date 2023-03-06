653f8d234aa8be9ebecb9b67c84305146b5420ef
IonutGaina <gainaionutz@gmail.com>
2023-03-06 10:06:15 +0200

    created pages, layouts, props and links

---
+++ larazillow/resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<!-- This is a template which we can use in different pages, in React we can say this is a component -->
+<template>
+    <Link href="/"> Main Page </Link> &nbsp;
+    <Link href="/hello"> Show Page </Link>
+
+    <slot></slot> <!-- Slot is used to put other tags from page to this layout -->
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
