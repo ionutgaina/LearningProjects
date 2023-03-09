599db68a1973a3701fe87f1a40248e6454093034
IonutGaina <gainaionutz@gmail.com>
2023-03-09 02:27:41 +0200

    persistent and default layouts in vue

--- larazillow/resources/js/Layouts/MainLayout.vue
+++ larazillow/resources/js/Layouts/MainLayout.vue
@@ -3,9 +3,19 @@
     <Link href="/"> Main Page </Link> &nbsp;
     <Link href="/hello"> Show Page </Link>
 
-    <slot></slot> <!-- Slot is used to put other tags from page to this layout -->
+    <div>
+        The page with time {{ timer }}
+    </div>
+
+    <slot>Default</slot> <!-- Slot is used to put other tags from page to this layout -->
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+
+// ref is used to create reactive variable
+const timer = ref(0)
+setInterval(() => timer.value++, 1000)
 </script>
--- larazillow/resources/js/Pages/Index/Index.vue
+++ larazillow/resources/js/Pages/Index/Index.vue
@@ -1,5 +1,4 @@
 <template>
-    <MainLayout>
         <div>
             Index
         </div>
@@ -9,15 +8,15 @@
         <div>
             The message is {{ message }}
         </div>
-    </MainLayout>
 </template>
 
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
-import MainLayout from '../../Layouts/MainLayout.vue'
 // defineProps is used to take props from controller ex IndexController.php in index method
 defineProps({
     message: String
 });
 </script>
+
+
